aa6d480f22d898e97cd4a661ce035c4aa32ccae7
Lorenzo-Mariano <hhumminah@gmail.com>
2022-07-27 00:18:18 +0800

    More lightweight

--- app.js
+++ app.js
@@ -3,43 +3,25 @@ let part2 = document.querySelector('.part2')
 let operatorField = document.querySelector('.operatorField')
 let numberButton = document.querySelectorAll('.num')
 let operatorButton = document.querySelectorAll('.operators')
+let backspace = document.querySelector('.backspace')
+let parentheses = document.querySelectorAll('.parenthesis')
 let clearButton = document.querySelector('.clear')
 let equalButton = document.querySelector('.equal')
 
 //Event Listeners
-numberButton.forEach(button => {
-    button.addEventListener('click', () => {
-        if (!operatorField.value) {
-            part1.value += button.innerText
-        } else {
-            part2.value += button.innerText
-        }
-    });
-});
+numberButton.forEach(button =>
+    button.addEventListener('click', () => part1.value += button.innerText)
+);
 
 operatorButton.forEach(button => {
-    button.addEventListener('click', () => operatorField.value = button.innerHTML);
+    button.addEventListener('click', () => part1.value += ` ${button.innerHTML} `);
 });
 
-clearButton.addEventListener('click', () => {
-    part1.value = '';
-    operatorField.value = '';
-    part2.value = '';
-})
+// how do i make it do the flashing line thing as i type?
+backspace.addEventListener('click', () => part1.value = part1.value.slice(0, part1.value.length - 1))
 
+clearButton.addEventListener('click', () => part1.value = '')
 
-equalButton.addEventListener('click', () => calculate(part1, operatorField, part2))
+parentheses.forEach(button => button.addEventListener('click', () => part1.value += button.innerText.toString()))
 
-// idk how this will go
-function calculate(top, operator, bottom) {
-    inputValue = `${top.value} ${operator.value} ${bottom.value}`
-    console.log(eval(inputValue))
-}
-
-// function typeNum() {
-
-// }
-
-
-
-
+equalButton.addEventListener('click', () => part1.value = eval(part1.value))
